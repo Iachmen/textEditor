70483726cd23507d75fa8cd4ab7ba874e5f2dd38
Iachmen <irinayachmen@mail.ru>
2017-11-26 17:51:18 +0100

    Add files via upload

---
+++ TestProject/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TextEditorBL;
+
+namespace TestProject
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main()
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			MainForm mainForm = new MainForm();
+			MessageService mService = new MessageService();
+			FileManager fManager = new FileManager();
+			MainPresenter mPresenter = new MainPresenter(mainForm, fManager, mService);
+			Application.Run(mainForm);
+		}
+	}
+}
---
+++ TextEditorBL/FileManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+namespace TextEditorBL
+{
+	public interface IFileManager
+	{
+		string GetContent(string filePath);
+		string GetContent(string filePath, Encoding encoding);
+		void SaveContent(string content, string filePath);
+		void SaveContent(string content, string filePath, Encoding encoding);
+		int GetSymbolCount(string count);
+		bool isExist(string filePath);
+	}
+	public class FileManager: IFileManager
+	{
+		private readonly Encoding _defaultEncoding = Encoding.GetEncoding(1251);
+
+		public bool isExist(string filePath)
+		{
+			return File.Exists(filePath);
+		}
+
+		public string GetContent(string filePath)
+		{
+			return GetContent(filePath, _defaultEncoding);
+		}
+
+		public string GetContent(string filePath, Encoding encoding)
+		{
+			string content = File.ReadAllText(filePath, encoding);
+			return content;
+		}
+
+		public void SaveContent(string content, string filePath)
+		{
+			SaveContent(content, filePath, _defaultEncoding);
+		}
+
+		public void SaveContent(string content, string filePath, Encoding encoding)
+		{
+			File.WriteAllText(filePath, content, encoding);
+		}
+
+		public int GetSymbolCount(string count)
+		{
+			return count.Length;
+		}
+
+	}
+}
